9d2dcf43360aba163430bcf33840527be119190e
EdgarKol <edgar.kollamaa@vikk.ee>
2022-10-13 16:11:54 +0300

    amen

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome from backend");
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
