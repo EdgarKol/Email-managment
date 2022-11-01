572b88d0cd4a969d0fabb34bcd9b0b2286aadc67
Edgar Kollamaa <edgar.kollamaa@vikk.ee>
2022-11-01 13:40:54 +0200

    changed swagger location to server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,13 @@ const bodyParser = require("body-parser");
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cors()); // Avoid CORS errors in browsers
+app.use(express.json()); // Populate req.body
+YAML = require("yamljs");
+const swaggerDocument = YAML.load("swagger.yaml");
+const swaggerUi = require("swagger-ui-express");
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("Welcome from backend");
