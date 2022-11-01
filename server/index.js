3bd60e6c8969ddc03bb24ae50cecff7a6ab3bb54
Edgar Kollamaa <edgar.kollamaa@vikk.ee>
2022-11-01 16:34:35 +0200

    get emails

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const port = 3000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const emails = [{"createdAt": "1999-12-31-00-00-00", "Name": "String", "content": "String"}]
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cors()); // Avoid CORS errors in browsers
@@ -13,7 +15,9 @@ const swaggerDocument = YAML.load("swagger.yaml");
 const swaggerUi = require("swagger-ui-express");
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+app.get('/emails',  (req, res) => {
+    res.send(emails);
+})
 app.get("/", (req, res) => {
   res.send("Welcome from backend");
 });
